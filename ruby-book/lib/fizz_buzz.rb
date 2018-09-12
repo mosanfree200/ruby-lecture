18f4cd8723ecc9eaa3d1cd4b9810f2501f32662d
mosanfree200 <hiroakiswim@outlook.jp>
2018-09-12 08:21:26 +0000

    add fizz_buzz

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,2 +1,15 @@
-def fizz_buzz(n)
-    n.to_s
+puts 'Hello world!'
+
+for n in 1..30
+    if n % 15 == 0
+        puts 'fizz buzz'
+    elsif n % 3 == 0
+        puts 'fizz'
+    elsif n % 5 == 0
+        puts 'buzz'
+    else 
+        puts n  
+    end
+end 
+        
+        
